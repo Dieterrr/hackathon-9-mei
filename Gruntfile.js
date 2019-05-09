b10d72d03076bda71cf661521de5cb4ceaee27d0
Dieter <dieter@yoast.com>
2019-05-09 16:41:13 +0200

    initial progress, not finished

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,39 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    "regex-replace": {
+      index: {
+          src: ['source-files/index.php'],
+          actions: [ 
+            {
+              name: 'update version in index.php',
+              search: 'Version:     \\d.\\d.\\d',
+              replace: 'Version:     <%= target %>',
+            }
+          ]
+      }
+
+      
+// source-files/classes/class.php
+// source-files/js/script.js
+  }
+
+    
   });
 
   // Load tasks here.
+  grunt.loadNpmTasks('grunt-regex-replace');
 
   // Define aliases here.
   grunt.registerTask('default', 'My default task description', function() {
     grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
+  });
+
+  // var target = grunt.option('target') || '1.0.0';
+  grunt.registerTask('update-version', 'My default task description', function(target) {
+    grunt.log.writeln( 'New version will be set to ' + target );
+    //grunt.task.run('regex-replace');
+
   });
 
 };
